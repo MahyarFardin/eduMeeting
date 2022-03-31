2e74ffbba2d68b5c10b55c5bad6c1ab19f5abfea
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-31 21:59:45 +0430

    centerings remianing

--- js/firstPage.js
+++ js/firstPage.js
@@ -1,7 +1,40 @@
-const next_prev = document.querySelector(".next-prev");
-var item_list=[]
+// this is for navbar
+window.addEventListener("scroll", (event) => {
 
-item_list.push()
-next_prev.addEventListener("click",() => {
+    const navbar = document.querySelector("nav");
 
-})
+    if (this.scrollY >= 985) {
+        navbar.classList.remove("bg-transparent", "navbar-light");
+        navbar.classList.add("navbar-dark", "bg-dark");
+    }
+    else {
+        navbar.classList.add("bg-transparent", "navbar-light");
+        navbar.classList.remove("navbar-dark", "bg-dark");
+    }
+});
+
+
+// this is for activie page in navbar
+const home = document.querySelector(".home");
+const contact = document.querySelector(".contact");
+const meetings = document.querySelector(".meetings");
+const apply = document.querySelector(".apply");
+
+var last = home;
+
+var Identifier= function(tag){
+    console.log(tag);
+    tag.target.classList.add("active");
+    last.classList.remove("active");
+    last=tag.target;
+}
+
+
+home.addEventListener("click",Identifier(home));
+
+contact.addEventListener("click",Identifier(contact));
+
+meetings.addEventListener("click",Identifier(meetings));
+
+apply.addEventListener("click",Identifier(apply));
+
