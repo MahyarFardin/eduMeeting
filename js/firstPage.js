67844ffe509205626c6ba1753aaef9dc5232135c
mahyarfardin <mahyarfardinfar@gmail.com>
2022-04-02 18:30:17 +0430

    stuck in navbar

--- js/firstPage.js
+++ js/firstPage.js
@@ -12,3 +12,14 @@ window.addEventListener("scroll", (event) => {
         navbar.classList.remove("navbar-dark", "bg-dark");
     }
 });
+
+var before="nav-link.active.home";
+
+for (let index = 0; index < document.querySelectorAll("nav li a").length; index++) {
+    document.querySelectorAll("nav li a")[index].addEventListener("click",(event)=>{
+        var dir=event.target.className;
+        document.querySelector("."+dir.slice(9, dir.length-1)).classList.add(".active");
+        document.querySelector(before).classList.remove(".active");
+        before=event.target.className;
+    })    
+}
